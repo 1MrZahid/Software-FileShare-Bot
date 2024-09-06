488713fa6d3baa39b921dc16266a26702da5b005
Mr Zahid <136788350+1MrZahid@users.noreply.github.com>
2024-09-07 00:43:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,6 +45,16 @@ def get_size(size):
         size /= 1024.0
     return "%.2f %s" % (size, units[i])
 
+# here is the user reply message 
+@Client.on_message(filters.private & filters.text)
+async def reply_to_random_messages(client, message: Message):
+    # Define the reply text
+    reply_text = "❌Don't send me messages!, @Tactition Has Blocked the Random Text."
+    
+    # Send the reply to the user
+    await message.reply(reply_text)
+
+
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if AUTH_CHANNEL:
